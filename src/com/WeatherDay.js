bc441c93344e0d2686d149b73855119ae1bb400e
danmiee <dan-mi@kakao.com>
2022-10-21 17:06:30 +0900

    기상청 중기정보 api 활용

---
+++ src/com/WeatherDay.js
@@ -0,0 +1,57 @@
+// main에서 받은 값에 따라 value 가져오기
+import w from '../db/weather.json';
+
+function WeatherDay(props) {
+  
+  const d = props.d;
+
+  // props에 따라 가져오는 값의 key가 달라지므로 변수 생성
+  const keys = [`rnSt${d}Am`, `rnSt${d}Pm`, `wf${d}Am`, `wf${d}Am`]
+
+  let witem = w.response.body.items.item[0];
+
+  // key에 해당하는 값만 노출하기
+  const items = [];
+  for (let k of keys) {
+    items.push(witem[k]);
+  }
+  console.log(items);
+
+  return ( 
+    <>
+      <ul className='detailUl'>
+        <li>
+          <span className='spanList'>
+            {d}일 후 오전 강수량
+          </span>
+          <span className='spanData'>
+            {items[0]}
+          </span>
+        </li>
+        <li>
+          <span className='spanList'>
+            {d}일 후 오후 강수량
+          </span>
+          <span className='spanData'>
+            {items[1]}
+          </span></li>
+        <li>
+          <span className='spanList'>
+            {d}일 후 오전 날씨예보
+          </span>
+          <span className='spanData'>
+            {items[2]}
+          </span></li>
+        <li>
+          <span className='spanList'>
+            {d}일 후 오전 날씨예보
+          </span>
+          <span className='spanData'>
+            {items[3]}
+          </span></li>
+      </ul>
+    </>
+  );
+}
+
+export default WeatherDay;
---
+++ src/com/WeatherMain.js
@@ -0,0 +1,38 @@
+// 원하는 정보의 key를 WeatherDay로 전송
+import WeatherDay from "./WeatherDay";
+import { useState } from "react";
+import './weather.css'
+
+function WeatherMain() {
+
+  // WeatherDay 속성으로 day부여 : 변경될 때 재랜더링 실행
+  let [day, setDay] = useState(3);
+
+  const handleClick = (d) => {
+    setDay(d);
+  }
+
+  return (
+    <>
+      <h1>날씨예보</h1>
+      <form className="btForm">
+        <button onClick={((e) => { e.preventDefault(); handleClick(3); })}>
+          3일 후 날씨
+        </button>
+        <button onClick={((e) => { e.preventDefault(); handleClick(4); })}>
+          4일 후 날씨
+        </button>
+        <button onClick={((e) => { e.preventDefault(); handleClick(5); })}>
+          5일 후 날씨
+        </button>
+        <button onClick={((e) => { e.preventDefault(); handleClick(6); })}>
+          6일 후 날씨
+        </button>
+      </form>
+      {/* 속성값 부여 > WeatherDay에서 props 사용가능 */}
+      <WeatherDay d={day} />
+    </>
+  );
+}
+
+export default WeatherMain;
